2201570313f58b3ab4cfbb3eaeca008794f7d26b
Shahar Solomianik <shahar@gnift.com>
2019-05-19 15:04:19 +0300

    added catch

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,42 @@ var router = express.Router();
 const users = require('../models/users')
 
 router.get('/', function(req, res, next) {
-  users.get(req.db).then((list) => {
+  users.get(req.db)
+  .then((list) => {
     res.json(list)
+  })
+  .catch((err) => {
+    res.send(500).status(err)
   })
 });
 
 router.post('/', function(req, res, next) {
-  users.add(req.db,req.body).then((user) => {
+  users.add(req.db,req.body)
+  .then((user) => {
     res.json(user)
   })
-  // res.json(user)
+  .catch((err) => {
+    res.send(500).status(err)
+  })
 });
 
 router.delete('/:id', function(req, res, next) {
-  users.delete(req.db,req.params.id).then((ret) => {
+  users.delete(req.db,req.params.id)
+  .then((ret) => {
     res.json(ret)
+  })
+  .catch((err) => {
+    res.send(500).status(err)
   })
 });
 
 router.patch('/password/:id', function(req, res, next) {
-  users.updatePassword(req.db,req.params.id,req.body.password).then((user) => {
+  users.updatePassword(req.db,req.params.id,req.body.password)
+  .then((user) => {
     res.json(user)
+  })
+  .catch((err) => {
+    res.send(500).status(err)
   })
 });
 
